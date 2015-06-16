f80ab981482196d9430b0b1696572fe5c7e94e0a
ThinkPad <ThinkPad@ThinkPad-PC>
2015-06-16 11:51:26 +0700

    Metro

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $('.dislay-ovelay').click(function () {
         $('.nav-box').toggleClass('open');
         $('.ovelay').fadeIn();
@@ -13,5 +14,49 @@ $(function () {
         $('.ovelay').removeClass('animated rollIn');
         $('.ovelay').addClass('animated hinge');
         $('.navbar-menu').toggleClass('open');
+    });
+
+    $('#img-fikly').click(function () {
+        $('.about').removeClass('riky');
+        $('.about').removeClass('reksi');
+        $('.about').addClass('fikly');
+        $('#img-fikly').addClass('active');
+        $('#img-riky').removeClass('active');
+        $('#img-reksi').removeClass('active');
+        $('#fikly').fadeIn()
+        $('#riky').hide();
+        $('#reksi').hide();
+    });
+    $('#img-riky').click(function () {
+        $('.about').removeClass('fikly');
+        $('.about').removeClass('reksi');
+        $('.about').addClass('riky');
+        $('#img-riky').addClass('active');
+        $('#img-fikly').removeClass('active');
+        $('#img-reksi').removeClass('active');
+        $('#riky').fadeIn();
+        $('#fikly').hide();
+        $('#reksi').hide();
+    });
+    $('#img-reksi').click(function () {
+        $('.about').removeClass('riky');
+        $('.about').removeClass('fikly');
+        $('.about').addClass('reksi');
+        $('#img-reksi').addClass('active');
+        $('#img-riky').removeClass('active');
+        $('#img-fikly').removeClass('active');
+        $('#reksi').fadeIn();
+        $('#riky').hide();
+        $('#fikly').hide();
+    });
+    jQuery(document).ready(function () {
+        jQuery('.box-skill').addClass("hidden").viewportChecker({
+            classToAdd: 'visible animated fadeInLeft',
+            offset: 200
+        });
+        jQuery('.box-portofolio').addClass("hidden").viewportChecker({
+            classToAdd: 'visible animated fadeIn',
+            offset: 170
+        });
     });
 });
